d949c5d9af07421bed1040ae091ad29b0d7cf93b
Fritalo <damienrzu@gmail.com>
2026-02-20 22:59:07 +0100

    input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 m_moveInput;
+    [SerializeField]
+    private Rigidbody2D m_rb;
+
+    private void Update()
+    {
+        #region Move
+
+        #endregion
+    }
+
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+
+    }
+
+    private void OnJump()
+    {
+
+    }
+
+    private void OnDash()
+    {
+
+    }
+
+    private void OnCrouch()
+    {
+
+    }
+
+
+}
